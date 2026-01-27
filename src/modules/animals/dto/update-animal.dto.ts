f60b17a8ab2308d9eae2f8cab6931f0aa597549d
Victor Ozoterio <victorozoterio@gmail.com>
2026-01-26 21:46:28 -0300

    fix(animals): permite atualizar status do animal

--- src/modules/animals/dto/update-animal.dto.ts
+++ src/modules/animals/dto/update-animal.dto.ts
@@ -1,4 +1,11 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { IsEnum, IsOptional } from 'class-validator';
+import { AnimalStatus } from '../utils';
 import { CreateAnimalDto } from './create-animal.dto';
 
-export class UpdateAnimalDto extends PartialType(CreateAnimalDto) {}
+export class UpdateAnimalDto extends PartialType(CreateAnimalDto) {
+  @IsOptional()
+  @IsEnum(AnimalStatus)
+  @ApiProperty({ enum: AnimalStatus })
+  status: AnimalStatus;
+}

91d811c8f6e23c6ade151cd1552a13b1faa6ff78
Victor Ozoterio <victorozoterio@gmail.com>
2026-02-05 20:01:50 -0300

    fix(libs): atualiza nome do bucket de produção da cloudflare

--- src/constants/cloudflare.constants.ts
+++ src/constants/cloudflare.constants.ts
@@ -3,7 +3,7 @@ import { Environment } from 'src/utils';
 const isDev = process.env.NODE_ENV === Environment.DEV;
 
 export const CLOUDFLARE = {
-  BUCKET_NAME: isDev ? 'friendly-snout-dev' : 'friendly-snout',
+  BUCKET_NAME: isDev ? 'friendly-snout-dev' : 'friendly-snout-prd',
   ENDPOINT: process.env.CLOUDFLARE_ENDPOINT as string,
   ACCESS_KEY_ID: process.env.CLOUDFLARE_ACCESS_KEY_ID as string,
   SECRET_ACCESS_KEY: process.env.CLOUDFLARE_SECRET_ACCESS_KEY as string,

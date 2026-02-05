5f64e4bbb6da526c4c49077213bc1a409b69dec4
Victor Ozoterio <victorozoterio@gmail.com>
2026-02-05 20:06:40 -0300

    docs(swagger): adiciona x-api-key

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -2,5 +2,6 @@ import { DocumentBuilder } from '@nestjs/swagger';
 
 export const swaggerConfig = new DocumentBuilder()
   .setTitle('Friendly Snout')
+  .addApiKey({ type: 'apiKey', name: 'x-api-key', in: 'header' }, 'x-api-key')
   .addBearerAuth({ type: 'http', name: 'bearer-token', in: 'header' }, 'bearer-token')
   .build();
